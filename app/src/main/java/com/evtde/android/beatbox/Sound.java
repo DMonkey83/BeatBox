3a56bb79950965546ed429e8dd7d076478247515
DMonkey83 <vilniseinars@gmail.com>
2015-11-04 12:53:12 +0000

    Initial commit

---
+++ app/src/main/java/com/evtde/android/beatbox/Sound.java
@@ -0,0 +1,33 @@
+package com.evtde.android.beatbox;
+
+/**
+ * Created by vilni on 18/10/2015.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+    private Integer mSoundId;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPat() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return  mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
+    }
+}
